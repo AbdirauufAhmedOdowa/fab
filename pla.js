361bd91de74c15b9a6e4983dce60a60ee088fc7e
Abdirauf ahmed <abdiraufahmedodowa@gmail.com>
2024-06-24 08:39:14 +0300

    my name

--- pla.js
+++ pla.js
@@ -49,7 +49,7 @@
 // })
 
 
-const light = document.querySelector("#light")
+/*const light = document.querySelector("#light")
 const onButton = document.querySelector("#onbtn")
 const offButton = document.querySelector("#offbtn")
 
@@ -59,4 +59,93 @@ onButton.addEventListener("click", function(){
 
 offButton.addEventListener("click", function(){
     light.style.backgroundColor = "black"
+})*/
+
+
+// const number = document.querySelector("#tiro")
+// const incerementButton =  document.querySelector("#incerementBtn")
+// const resetBtn =  document.querySelector("#resetBtn")
+// const nonBtn = document.querySelector("#nonBtn")
+
+// let initialvalue = 4
+
+// incerementButton.addEventListener("click", function(){
+//     initialvalue++
+//     number.innerHTML = initialvalue
+
+//     if (initialvalue ==4){
+//         number.style.backgroundColor= "red"
+//     }
+//     else if (initialvalue ==5){
+//         number.style.backgroundColor= "yellow"
+//     }
+//     else if (initialvalue ==6){
+//         number.style.backgroundColor= "gray"
+//     }
+//     else if (initialvalue ==7){
+//         number.style.backgroundColor= "green"
+//     }
+//     else if (initialvalue ==8){
+//         number.style.backgroundColor= "purple"
+//     }
+//     else if (initialvalue ==9){
+//         number.style.backgroundColor= "white"
+//     }
+    
+// })
+
+
+// nonBtn.addEventListener("click", function(){
+//     initialvalue--
+//     number.innerHTML = initialvalue
+// })
+
+// //reset button
+
+// resetBtn.addEventListener("click", function(){
+//     initialvalue = 0
+//     number.innerHTML = initialvalue
+// })
+
+
+const me = document.querySelector("#tiro")
+const you = document.querySelector("#incerementBtn")
+const mac = document.querySelector("#nonBtn")
+const we = document.querySelector("#resetBtn")
+
+let initialvalue = 0
+
+you.addEventListener("click", function(){
+    initialvalue++
+    me.innerHTML = initialvalue
+
+    if(initialvalue ==3){
+        me.style.backgroundColor = "green"
+        me.style.color = "white"
+        
+    }
+    else if(initialvalue ==4){
+        me.style.backgroundColor = "yellow"
+        me.style.color = "red"
+    }
+
+    else
+    me.style.backgroundColor = "white"
+    me.style.color = "black"
 })
+mac.addEventListener("click", function(){
+    initialvalue--
+    me.innerHTML = initialvalue
+})
+
+we.addEventListener("click", function(){
+    initialvalue=0
+    me.innerHTML = initialvalue
+})
+
+
+
+
+
+
+
